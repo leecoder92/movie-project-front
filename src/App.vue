d50cd88229800a7bfd4110f01868eccb65dfe6c4
leesk3732 <leesk3732@naver.com>
2021-11-18 14:18:45 +0900

    login

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,49 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
         <router-link class="nav-link link-dark" to="/">프로젝트</router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/">영화</router-link>
+              <router-link
+                class="nav-link active"
+                aria-current="page"
+                to="/"
+              />영화
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/recommendation">추천</router-link>
+              <router-link
+                class="nav-link active"
+                aria-current="page"
+                to="/recommendation"
+              />추천
             </li>
             <li class="nav-item">
-              <router-link class="nav-link active" aria-current="page" to="/community">커뮤니티</router-link>
+              <router-link
+                class="nav-link active"
+                aria-current="page"
+                to="/community"
+              />커뮤니티
             </li>
           </ul>
         </div>
+        <span>
+          <router-link :to="{ name: 'Signup' }"> 회원가입 </router-link>
+          <router-link :to="{ name: 'Login' }"> 로그인 </router-link>
+        </span>
       </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+<<<<<<< Updated upstream
 import Home from '../views/movies/Home.vue'
 import Recommendation from '@/views/movies/Recommendation.vue'
 import Community from '@/views/movies/Community.vue'
+=======
+import Home from '../views/Home.vue'
+import Recommendation from '@/views/Recommendation.vue'
+import Community from '@/views/Community.vue'
+import Signup from '@/views/accounts/Signup'
+import Login from '@/views/accounts/Login'
+>>>>>>> Stashed changes
 
 Vue.use(VueRouter)
 
@@ -10,24 +18,34 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
     path: '/recommendation',
     name: 'Recommendation',
-    component: Recommendation
+    component: Recommendation,
   },
   {
     path: '/community',
     name: 'Community',
-    component: Community
+    component: Community,
+  },
+  {
+    path: '/accounts/signup',
+    name: 'Signup',
+    component: Signup,
+  },
+  {
+    path: '/accounts/login',
+    name: 'Login',
+    component: Login,
   },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
 
 export default router
